d661156c95dde69bfd525c52e323ab64e676c7e7
JaySanchez0 <jeisson.sanchez@mail.escuelaing.edu.co>
2020-09-17 11:12:06 -0500

    Correccion

--- src/main/java/com/eci/tacs/testers/TesterImpl.java
+++ src/main/java/com/eci/tacs/testers/TesterImpl.java
@@ -64,36 +64,35 @@ public class TesterImpl implements Tester {
     public void addReserva(String username) throws TestException {
         if (webDriver == null) throw new TestException(TestException.DRIVER_NOT_SETUP);
         Notifier.addNotification("PRUEBA DE RESERVA DE EQUIPO :");
-        WebElement elm = new WebDriverWait(webDriver, 20).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/aside/nav/ul/li[2]")));
-        //WebElement elm = webDriver.findElement(By.xpath("/html/body/aside/nav/ul/li[2]"));
+        //Va a la seccion de ver recursos disponibles
+        WebElement elm = element(By.xpath("/html/body/aside/nav/ul/li[2]"));
+        //Ingresa a la seccion ver recursos disponibles
         elm.click();
-        //WebElement elm2 = webDriver.findElement(By.xpath("/html/body/aside/nav/ul/li[2]/ul/li"));
-        //WebElement elm2 = new WebDriverWait(webDriver, 20).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/aside/nav/ul/li[2]/ul/li")));
-        //elm2.click();
-        elm = new WebDriverWait(webDriver, 20).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/section/div/div[1]/div[2]/div/table/tbody/tr[1]/td[7]")));
-        //elm = webDriver.findElement(By.xpath("/html/body/section/div/div[1]/div[2]/div/table/tbody/tr[1]/td[7]/center"));
+        //selecciona la el boton de reserva para el primer item
+        elm =  element(By.xpath("/html/body/section/div/div[1]/div[2]/div/table/tbody/tr[1]/td[7]"));
+        //Da click para habilitar el boton
         elm.click();
-        elm = new WebDriverWait(webDriver, 20).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/section/div/div[1]/div[2]/div/table/tbody/tr[2]/td/center/form/button")));
-        //elm = webDriver.findElement(By.xpath("//*[@id=\"j_idt39:536:j_idt41:boton1\"]"));
+        //Al cargar la vista selecciona el boton para realizar la reserva
+        elm = element(By.xpath("/html/body/section/div/div[1]/div[2]/div/table/tbody/tr[2]/td/center/form/button"));
         elm.click();
-        ///html/body/section/form/table/tbody/tr/td/div/div/div[2]/div/table/tbody/tr/td/div/div/div[2]/div[1]/table/tbody/tr/td[2]
-        elm = new WebDriverWait(webDriver, 20).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("html/body/section/form/table/tbody/tr/td/div/div/div[2]/div/table/tbody/tr/td/div/div/div[2]/div[1]/table/tbody/tr/td[2]")));
+        //Selecciona en el calendario la fecha 07/09/2020
+        elm = element(By.xpath("html/body/section/form/table/tbody/tr/td/div/div/div[2]/div/table/tbody/tr/td/div/div/div[2]/div[1]/table/tbody/tr/td[2]"));
+        //Abre el model de registro de la reserva
         elm.click();
-        elm = new WebDriverWait(webDriver, 20).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("html/body/section/form/table/tbody/tr/td/div/div/div[2]/div/table/tbody/tr/td/div/div/div[2]/div[1]/table/tbody/tr/td[2]")));
-        //elm.click();
-        ///html/body/section/form/div[2]/div[2]/table/tbody/tr[1]/td[2]/input
+        //Ingresa el nombre de la reserva
         elm = new WebDriverWait(webDriver, 20).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("html/body/section/form/div[2]/div[2]/table/tbody/tr[1]/td[2]/input")));
         elm.click();
         elm.sendKeys("Test-App");
-        elm = new WebDriverWait(webDriver, 20).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("html/body/section/form/div[2]/div[2]/table/tbody/tr[5]/td[2]/select")));
+        //Selecciona la cantidad de recursos a reservar(Libros)
+        elm = webDriver.findElement(By.xpath("html/body/section/form/div[2]/div[2]/table/tbody/tr[5]/td[2]/select"));
         elm.sendKeys("1");
-        /////html/body/section/form/div[2]/div[2]/table/tbody/tr[5]/td[2]/select
-
+        //Ejecuta la reserva
         elm = webDriver.findElement(By.xpath("/html/body/section/form/div[2]/div[2]/table/tbody/tr[6]/td[2]/button"));
         elm.click();
+        //Actualiza la pagina
         webDriver.navigate().refresh();
-        ////*[@id="j_idt39:schedule_container"]/div[2]/div/table/tbody/tr/td/div/div/div[3]/div[2]/table/tbody/tr/td[4]/a/div/span[2]
-        elm = new WebDriverWait(webDriver, 20).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/section/form/table/tbody/tr/td/div/div/div[2]/div/table/tbody/tr/td/div/div/div[3]/div[2]/table/tbody/tr/td[4]/a/div/span[2]")));
+        //Verifica que dentro de el calendario se encuentre la reserva
+        elm = element(By.xpath("/html/body/section/form/table/tbody/tr/td/div/div/div[2]/div/table/tbody/tr/td/div/div/div[3]/div[2]/table/tbody/tr/td[4]/a/div/span[2]"));
         String text = elm.getText();
         if (text.contains(username)) {
             Notifier.addNotification("La reserva se realizó exitosamente \n");
